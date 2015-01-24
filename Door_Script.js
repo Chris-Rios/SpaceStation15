a630126f86718991fcca5ab6e9adeb11c1e908d8
unknown <Administrator@W213-25.academic.raritanval.edu>
2015-01-24 03:10:55 -0500

    Add sensor script

--- Door_Script.js
+++ Door_Script.js
@@ -8,42 +8,28 @@ var open : boolean;
 var starting_position : float;
 
 function Start () {
-	OpenDoor();
 	starting_position = rigidbody.position.z;
-	unlocked = true;
+	//unlocked = true;
 }
 
 //When the player collides with the door, it will open if unlocked
-function OnCollisionEnter (other: Collision) {
-		Debug.Log("collided!");
-		Debug.Log(unlocked);
-		if(unlocked && other.gameObject.tag == "Player"){
-			open = true;
-			//gameObject.SetActive(false);
-			rigidbody.position.z +=5;
-		}	
-			
-}
 
-function OnCollisionExit (other: Collision){
-		Debug.Log("left");
-		if(open && other.gameObject.tag == "Player"){
-			open = false;
-			//gameObject.SetActive(true);
-			rigidbody.position.z -=5;
-
-		}
-}
 
 //called by a controller to open the door when a puzzle is completed
 function OpenDoor(){
-	open = true;
 	
+	Debug.Log("opening");
+	if(unlocked == true){
+		open = true;
+		rigidbody.position.z +=5;
+	}
 }
 
 function CloseDoor(){
-
-	
+	Debug.Log("closing");
+	if(unlocked == true){
+		rigidbody.position.z -=5;
+	}
 }
 
 function MoveDoor(){
---
+++ Sensor_Script.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var Door : GameObject;
+
+function OnTriggerEnter (other: Collider) {
+		if(other.tag == "Player"){
+			Door.SendMessage("OpenDoor");
+		}	
+			
+}
+
+function OnTriggerExit (other: Collider){
+		if(other.gameObject.tag == "Player"){
+			Door.SendMessage("CloseDoor");
+
+		}
+}
