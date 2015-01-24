124f565bc69de09ecfb98ab30bd663ec74b852c3
unknown <Administrator@W213-25.academic.raritanval.edu>
2015-01-24 02:19:10 -0500

    Add Door Script and Player Movement Script

---
+++ Door_Script.js
@@ -0,0 +1,52 @@
+﻿#pragma strict
+
+
+
+
+var unlocked : boolean;
+var open : boolean;
+var starting_position : float;
+
+function Start () {
+	OpenDoor();
+	starting_position = rigidbody.position.z;
+	unlocked = true;
+}
+
+//When the player collides with the door, it will open if unlocked
+function OnCollisionEnter (other: Collision) {
+		Debug.Log("collided!");
+		Debug.Log(unlocked);
+		if(unlocked && other.gameObject.tag == "Player"){
+			open = true;
+			//gameObject.SetActive(false);
+			rigidbody.position.z +=5;
+		}	
+			
+}
+
+function OnCollisionExit (other: Collision){
+		Debug.Log("left");
+		if(open && other.gameObject.tag == "Player"){
+			open = false;
+			//gameObject.SetActive(true);
+			rigidbody.position.z -=2;
+
+		}
+}
+
+//called by a controller to open the door when a puzzle is completed
+function OpenDoor(){
+	open = true;
+	
+}
+
+function CloseDoor(){
+
+	
+}
+
+function MoveDoor(){
+
+	
+}
---
+++ Player_Script.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+var speed : float;
+
+function FixedUpdate(){
+	
+	var moveHorizontal = Input.GetAxis("Horizontal");
+	var moveVertical = Input.GetAxis("Vertical");
+	var movement = new Vector3(moveHorizontal,0,moveVertical);
+	//rigidbody.AddForce(movement * speed * Time.deltaTime);
+	rigidbody.velocity = movement * speed;
+}
